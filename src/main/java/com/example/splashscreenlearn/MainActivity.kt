a4db00fcb7da8c5c0d65d87349cd4ff335692b96
Victor Dias <victordiasdeveloper@gmail.com>
2021-09-03 10:43:44 -0300

    Initial commit

---
+++ src/main/java/com/example/splashscreenlearn/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.splashscreenlearn
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.splashscreenlearn.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
+
+    }
+}
